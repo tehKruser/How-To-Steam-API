b0a110ac8d099734b57a95c60f29654f0345a8ed
tehKruser <krusej@oregonstate.edu>
2016-11-12 13:58:01 -0700

    Getting the server to work
    Updates to script-final-page

--- script-final-page.js
+++ script-final-page.js
@@ -7,14 +7,30 @@ var query3= "&relationship=friend";
 var querystring = query1 + apiKey + query2 + steamID + query3;
 //console.log(querystring);
 
+/*
 var req = new XMLHttpRequest();
-req.open(method, "https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=90E48644726077A5E93BCC32953DCF46&steamid=76561197966297270&relationship=friend", true);
+req.open(method, "http://flip2.engr.oregonstate.edu:8228", true);
 req.setRequestHeader('Content-Type', 'application/json');
+req.setRequestHeader('Access-Control-Allow-Origin', '*');
+req.send(null);
 req.addEventListener('load', function(){
   var response = JSON.parse(req.responseText);
   console.log(response);
 	}
 );
+*/
+
+var req = new XMLHttpRequest();
+req.open("get", 'http://flip2.engr.oregonstate.edu:8228', true);
+req.addEventListener('load', function() {
+  if(req.status >= 200 && req.status < 400) {
+    var response = JSON.parse(req.responseText);
+    //var numberOfResults = response.totalResults;
+    console.log(response);
+  } else {
+    console.log("Error in network request: " + req.statusText);
+  }
+});
 req.send(null);
 
 /*
